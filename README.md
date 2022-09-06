b2b2e1dc09f8526331eefca557d49c9267e3695e
andrewperez <andrewprz5@gmail.com>
2022-09-05 20:36:27 -0400

    Program Complete

--- README.md
+++ README.md
@@ -1,11 +1,10 @@
 # Rock-Paper-and-Scissors
 
-This is a game of Rock, Paper and Scissors. Most of this program is relied on Javascript 
-since functions are needed to allow the user to a decision and use one of the three to 
-throw against the computer. The computer also has the ability to decide which of the three 
-to choose in order to counteract what the user threw down. The winner is determined after 
-a single round, and if the user wishes, they can choose to go another round with the 
-computer. There will be no HTML/CSS input as it is not required for this project, but later
+This is a game of Rock, Paper and Scissors. Most of this program is relied on Javascript
+since functions are needed to allow the user to a decision and use one of the three to
+throw against the computer. The computer also has the ability to decide which of the three
+to choose in order to counteract what the user threw down. The winner is determined after
+a 5 total rounds, and if the user wishes, they can choose to enter another match with the
+computer. There will be no CSS input as it is not required for this project, but later
 on, at some point, there will be a user-based interaction. As of now, this can be played
-in the Console on the website's Developer Tools. 
-
+in the Console on the website's Developer Tools.
---
+++ index.js
@@ -0,0 +1,92 @@
+let playerScore = 0;
+let computerScore = 0;
+
+function getComputerChoice() {
+    let randomNumber = Math.floor(Math.random() * 3);
+    if (randomNumber == 0) {
+        return "rock";
+    } else if (randomNumber == 1) {
+        return "paper";
+    } else {
+        return "scissors";
+    }
+};
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection == "rock" && computerSelection == "rock") {
+        alert("Player chooses Rock. Computer chooses Rock. It's a tie!");
+        playerScore += 1;
+        computerScore += 1;
+    } else if (playerSelection == "paper" && computerSelection == "rock") {
+        alert("Player chooses Paper. Computer chooses Rock. Paper covers Rock. Player wins!");
+        playerScore += 1;
+    } else if (playerSelection == "scissors" && computerSelection == "rock") {
+        alert("Player chooses Scissors. Computer chooses Rock. Rock crushes Scissors. Computer wins!");
+        computerScore += 1;
+    } else if (playerSelection == "rock" && computerSelection == "paper") {
+        alert("Player chooses Rock. Computer chooses Paper. Paper covers Rock. Computer wins!");
+        computerScore += 1;
+    } else if (playerSelection =="rock" && computerSelection == "scissors") {
+        alert("Player chooses Rock. Computer chooses Scissors. Rock crushes Scissors. Player wins!");
+        playerScore += 1;
+    } else if (playerSelection == "paper" && computerSelection == "paper") {
+        alert("Player chooses Paper. Computer chooses Paper. It's a tie!");
+        playerScore += 1;
+        computerScore += 1;
+    } else if (playerSelection == "scissors" && computerSelection == "paper") {
+        alert("Player chooses Scissors. Computer chooses Paper. Scissors cuts Paper. Player wins!");
+        playerScore += 1;
+    } else if (playerSelection == "paper" && computerSelection == "scissors") {
+        alert("Player chooses Paper. Computer chooses Scissors. Scissors cuts Paper. Computer wins!");
+        computerScore += 1;
+    } else if (playerSelection == "scissors" && computerSelection == "scissors") {
+        alert("Player chooses Scissors. Computer chooses Scissors. It's a tie!");
+        playerScore += 1;
+        computerScore += 1;
+    } else {
+        alert("That ain't an option dumbo.");
+        i -= 1;
+    }
+};
+
+function game() {
+    if (confirm("Welcome to Rock, Paper, & Scissors! Dare to challenge the computer for 5 rounds?")) {
+        for (i = 0; i < 5; i++) {
+            let playerInput = prompt("Pick your choice: Rock, Paper, or Scissors?");
+            let computerChoice = getComputerChoice();
+            if (playerInput) {
+                let playerChoice = playerInput.toLowerCase();
+                playRound(playerChoice, computerChoice);
+                alert("Player: " + playerScore + " Computer: " + computerScore);
+                if (i == 4 && (playerScore > computerScore)) {
+                    alert("You won!");
+                } else if (i == 4 && (playerScore < computerScore)) {
+                    alert("Computer won!");
+                } else if (i == 4 && (playerScore == computerScore)) {
+                    alert("No winner determined!");
+                } else {
+                }
+            } else {
+                alert("Quitter..."); 
+                break;       
+            }
+        }
+    } else {
+        alert("Youse a scaredy cat...")
+    }
+};
+
+function playAgain() {
+    if (confirm("Do you wish to play again?")) {
+        game();
+    } else {
+        alert("Ok, bye bye then!");
+    }
+};
+
+game();
+playAgain();
+
+
+
+// shorten entire code if you can
