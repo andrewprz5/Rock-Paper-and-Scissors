cf7232e19a1c87a18c42a12e7882431eafb0873c
andrewperez <andrewprz5@gmail.com>
2022-10-31 20:56:06 -0400

    UI Design and Add E-Lstnrs

--- index.js
+++ index.js
@@ -13,82 +13,74 @@ function getComputerChoice() {
 };
 
 function playRound(playerSelection, computerSelection) {
-    if (playerSelection == "rock" && computerSelection == "rock") {
-        alert("Player chooses Rock. Computer chooses Rock. It's a tie!");
+    if (playerSelection == "rock" && computerSelection == "rock" || 
+    playerSelection == "paper" && computerSelection == "paper" || 
+    playerSelection == "scissors" && computerSelection == "scissors") {
         playerScore += 1;
         computerScore += 1;
-    } else if (playerSelection == "paper" && computerSelection == "rock") {
-        alert("Player chooses Paper. Computer chooses Rock. Paper covers Rock. Player wins!");
+    } else if (playerSelection == "paper" && computerSelection == "rock" ||
+    playerSelection =="rock" && computerSelection == "scissors" || 
+    playerSelection == "scissors" && computerSelection == "paper") {
         playerScore += 1;
-    } else if (playerSelection == "scissors" && computerSelection == "rock") {
-        alert("Player chooses Scissors. Computer chooses Rock. Rock crushes Scissors. Computer wins!");
-        computerScore += 1;
-    } else if (playerSelection == "rock" && computerSelection == "paper") {
-        alert("Player chooses Rock. Computer chooses Paper. Paper covers Rock. Computer wins!");
-        computerScore += 1;
-    } else if (playerSelection =="rock" && computerSelection == "scissors") {
-        alert("Player chooses Rock. Computer chooses Scissors. Rock crushes Scissors. Player wins!");
-        playerScore += 1;
-    } else if (playerSelection == "paper" && computerSelection == "paper") {
-        alert("Player chooses Paper. Computer chooses Paper. It's a tie!");
-        playerScore += 1;
-        computerScore += 1;
-    } else if (playerSelection == "scissors" && computerSelection == "paper") {
-        alert("Player chooses Scissors. Computer chooses Paper. Scissors cuts Paper. Player wins!");
-        playerScore += 1;
-    } else if (playerSelection == "paper" && computerSelection == "scissors") {
-        alert("Player chooses Paper. Computer chooses Scissors. Scissors cuts Paper. Computer wins!");
-        computerScore += 1;
-    } else if (playerSelection == "scissors" && computerSelection == "scissors") {
-        alert("Player chooses Scissors. Computer chooses Scissors. It's a tie!");
-        playerScore += 1;
+    } else if (playerSelection == "scissors" && computerSelection == "rock" ||
+    playerSelection == "rock" && computerSelection == "paper" || 
+    playerSelection == "paper" && computerSelection == "scissors") {
         computerScore += 1;
     } else {
-        alert("That is not an option, buddy.");
         i -= 1;
     }
 };
 
-function game() {
-    if (confirm("Welcome to Rock, Paper, & Scissors! Dare to challenge the computer for 5 rounds?")) {
-        for (i = 0; i < 5; i++) {
-            let playerInput = prompt("Pick your choice: Rock, Paper, or Scissors?");
-            let computerChoice = getComputerChoice();
-            if (playerInput) {
-                let playerChoice = playerInput.toLowerCase();
-                playRound(playerChoice, computerChoice);
-                alert("Player: " + playerScore + " Computer: " + computerScore);
-                if (i == 4 && (playerScore > computerScore)) {
-                    alert("You won!");
-                } else if (i == 4 && (playerScore < computerScore)) {
-                    alert("Computer won!");
-                } else if (i == 4 && (playerScore == computerScore)) {
-                    alert("No winner determined!");
-                } else {
-                }
-            } else {
-                alert("Quitter..."); 
-                break;       
-            }
+const h1 = document.querySelector("h1");
+const btns = document.querySelectorAll('button');
+const container = document.querySelector('.results');
+const main = document.querySelector('.container');
+const results = document.createElement("p");
+const playAgain = document.createElement('button');
+const rock = document.getElementById('rock');
+const paper = document.getElementById('paper');
+const scissors = document.getElementById('scissors');
+const h3 = document.createElement("h3");
+playAgain.innerHTML = 'Play Again?';
+playAgain.style.cssText = 'background-color: #1e90ff; color: white; border-radius: 60px; padding: 10px; height: 150px; width: 250px; font-size: 40px;' 
+results.textContent = 'Player Score: 0 Computer Score: 0';
+
+btns.forEach((button) => {
+    button.addEventListener('click', () => {
+        playRound(button.id, getComputerChoice());
+        results.textContent = 'Player Score: ' + playerScore + ' Computer Score: ' + computerScore + ''; 
+        container.appendChild(results);
+        if (playerScore == 5 && playerScore > computerScore) {
+            h3.innerHTML = 'Player Wins!';
+            switchToButton();
+        } else if (computerScore == 5 && computerScore > playerScore) {
+            h3.innerHTML = 'Computer Wins!';
+            switchToButton();
+        } else if (playerScore == 5 && computerScore == 5) {
+            h3.innerHTML = `It's a tie!`;
+            switchToButton();
         }
-    } else {
-        alert("So you're a chicken then, huh?");
-    }
+    });
+});
+
+
+playAgain.addEventListener('click', () => {
+    results.textContent = 'Player Score: 0 Computer Score: 0';
+    main.removeChild(playAgain);
+    main.appendChild(rock);
+    main.appendChild(paper);
+    main.appendChild(scissors);
+    main.removeChild(h3);
+    main.style['flex-direction'] = "row";
+});
+
+function switchToButton() {
+    main.style['flex-direction'] = "column";
+    main.appendChild(h3);
+    main.appendChild(playAgain);
+    playerScore = 0;
+    computerScore = 0;
+    rock.remove();
+    paper.remove();
+    scissors.remove();
 };
-
-function playAgain() {
-    for (i = 0; i < Infinity; i++) {
-        if (confirm("Do you wish to play again?")) {
-            playerScore = 0;
-            computerScore = 0;
-            game();
-        } else {
-            alert("Ok, see ya later!");
-            break;
-        }
-    }
-};
-
-game();
-playAgain();
-
